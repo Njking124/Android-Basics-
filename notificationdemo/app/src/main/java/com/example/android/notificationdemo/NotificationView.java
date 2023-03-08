dad5bf127c52f2b776ba6b5d1dd23b1a1e639250
“nitesh <jaiswarnitesh124@gmail.com>
2023-03-08 22:25:42 +0530

    added

---
+++ notificationdemo/app/src/main/java/com/example/android/notificationdemo/NotificationView.java
@@ -0,0 +1,13 @@
+package com.example.android.notificationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_view);
+    }
+}
